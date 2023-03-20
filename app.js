aa2bfeacf9c6b4ef1fc4a7f21fc24ba1b846af48
ded <dedlytbusiness@gmail.com>
2023-03-20 14:02:10 +0000

    v1

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/bye", (req, res) => {
+  res.send("goodbye!")
+})
+
+app.get("/greeting", (req, res) => {
+    res.send(`hi, ${req.query.name}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
